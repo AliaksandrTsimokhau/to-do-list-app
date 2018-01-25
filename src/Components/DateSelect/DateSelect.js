dc6ab0273a88ac32db58146c57c2523e194b3363
AliaksandrTsimokhau <alextimohov@gmail.com>
2018-01-25 14:20:30 +0300

    priority added to addtaskform

---
+++ src/Components/DateSelect/DateSelect.js
@@ -0,0 +1,37 @@
+import React, {Component} from 'react';
+import './AddTaskForm.css';
+import SearchInput from './SearchInput/SearchInput.js';
+import PrioritySelect from './PrioritySelect.js';
+import DateSelect from '../DateSelect/DateSelect.js';
+import PropTypes from 'prop-types';
+
+export class AddTaskForm extends Component {
+  onSubmit(ev){
+    ev.preventDefault();
+    let formData = [...ev.target.querySelectorAll('[name]')]
+    .reduce((hash,item) =>({
+      ...hash,
+      [item.getAttribute('name')]: item.value
+    }),{}); //что значат пустые фигурные скобки?
+    this.props.onSubmit(formData);
+    ev.target.reset();
+  }
+
+  render(){
+    return(
+      <form className="toDoListForm" onSubmit={this.onSubmit.bind(this)}>
+        <fieldset>
+          <legend>{this.props.title}</legend>
+          <SearchInput name="title" title="Title"/>
+          <PrioritySelect name="priority" />
+          <DateSelect name="date" title="Date" />
+          <textarea name="descript" placeholder="Description" />
+          <input type="submit" class="submit" value="Add"></input>
+        </fieldset>
+      </form>
+    )
+  }
+}
+AddTaskForm.propTypes = {
+  title: PropTypes.string
+};
